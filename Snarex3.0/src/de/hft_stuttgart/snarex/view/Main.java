1d790be2666d199859064d077e01604a6fe924cf
Adrian Nahlik <71naad1bwi@hft-stuttgart.de>
2018-01-13 15:20:41 +0100

    Gui created, controller and main modified.

--- Snarex3.0/src/de/hft_stuttgart/snarex/view/Main.java
+++ Snarex3.0/src/de/hft_stuttgart/snarex/view/Main.java
@@ -1,5 +1,28 @@
 package de.hft_stuttgart.snarex.view;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		BorderPane outerPane = (BorderPane) FXMLLoader.load(ClassLoader.getSystemResource("de/hft_stuttgart/snarex/view/View.fxml")); 
+		primaryStage.setScene(new Scene(outerPane));
+		primaryStage.setTitle("Snarex");
+		primaryStage.show();
+		
+		
+	}
+	
+	
+	
+	public static void main (String[]args) {
+		launch(args);
+	}
 
 }
